54fe5b54e27bef2eb64d2d67bed571aec9e7a654
yupix <yupi0982@outlook.jp>
2022-04-29 02:57:06 +0900

    feat: エラーが起きてもスクショを撮るように

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
+import functools
 import os
 import time
 import requests
 from playwright.sync_api import Page
 import yaml
+
+
+class LoginError:
+    """ログイン時にエラーが発生しました"""
 
 if not os.path.isdir('./ScreenShots'):
     os.makedirs('./ScreenShots')
@@ -19,18 +24,30 @@ except FileNotFoundError:
     config = open_file('../.config/test.yml')
 
 
+def detect_error(func):
+    @functools.wraps(func)
+    def wrapper(page: Page):
+        try:
+            func(page)
+        except Exception as e:
+            page.screenshot(path="./ScreenShots/error.png")
+            raise LoginError(f'ログイン時にエラーが発生しました\n{e}')
+
 def login(page: Page):
-    page.goto(f'{config["url"]}')
-    page.wait_for_load_state('networkidle')
-    page.click('//*[@id="app"]/main/div/div[1]/div/p/span[3]')
-    page.query_selector(
-        '#app > div.modal.v--modal-overlay.scrollable > div > div.v--modal-box.v--modal > form > div.normal-signin > div:nth-child(1) > div.input > input[type=text]').fill('alice')
-    page.query_selector(
-        '#app > div.modal.v--modal-overlay.scrollable > div > div.v--modal-box.v--modal > form > div.normal-signin > div:nth-child(2) > div.input > input[type=password]').fill('alice1234')
-    page.keyboard.press('Enter')
+    try:
+        page.goto(f'{config["url"]}')
+        page.wait_for_load_state('networkidle')
+        page.click('//*[@id="app"]/main/div/div[1]/div/p/span[3]')
+        page.query_selector(
+            '#app > div.modal.v--modal-overlay.scrollable > div > div.v--modal-box.v--modal > form > div.normal-signin > div:nth-child(1) > div.input > input[type=text]').fill('alice')
+        page.query_selector(
+            '#app > div.modal.v--modal-overlay.scrollable > div > div.v--modal-box.v--modal > form > div.normal-signin > div:nth-child(2) > div.input > input[type=password]').fill('alice1234')
+        page.keyboard.press('Enter')
+    except Exception as e:
+        page.screenshot(path="./ScreenShots/error.png")
+        raise LoginError(f'ログイン時にエラーが発生しました\n{e}')
     time.sleep(3)
     return page
-
 
 def test_create_user(page: Page):
     count: int = 0
@@ -41,19 +58,24 @@ def test_create_user(page: Page):
         except requests.exceptions.ConnectionError:
             time.sleep(3)
             count += 1
-    page.goto(f'{config["url"]}')
-    page.wait_for_load_state('networkidle')
-    page.click('//*[@id="app"]/main/div/div[1]/div/p/span[1]')
-    # アカウント登録
-    page.query_selector(
-        '#app > div.modal.v--modal-overlay.scrollable > div > div.v--modal-box.v--modal > form > div:nth-child(1) > div.input > input[type=text]').fill('alice')
-    page.query_selector(
-        '#app > div.modal.v--modal-overlay.scrollable > div > div.v--modal-box.v--modal > form > div:nth-child(2) > div.input > input[type=password]').fill('alice1234')
-    page.query_selector(
-        '#app > div.modal.v--modal-overlay.scrollable > div > div.v--modal-box.v--modal > form > div:nth-child(3) > div.input > input[type=password]').fill('alice1234')
-    page.query_selector('#app > div.modal.v--modal-overlay.scrollable > div > div.v--modal-box.v--modal > form > button').click()
-    time.sleep(3)
-    page.screenshot(path="./ScreenShots/register_user.png")
+    try:
+        page.goto(f'{config["url"]}')
+        page.wait_for_load_state('networkidle')
+        page.click('//*[@id="app"]/main/div/div[1]/div/p/span[1]')
+        # アカウント登録
+        page.query_selector(
+            '#app > div.modal.v--modal-overlay.scrollable > div > div.v--modal-box.v--modal > form > div:nth-child(1) > div.input > input[type=text]').fill('alice')
+        page.query_selector(
+            '#app > div.modal.v--modal-overlay.scrollable > div > div.v--modal-box.v--modal > form > div:nth-child(2) > div.input > input[type=password]').fill('alice1234')
+        page.query_selector(
+            '#app > div.modal.v--modal-overlay.scrollable > div > div.v--modal-box.v--modal > form > div:nth-child(3) > div.input > input[type=password]').fill('alice1234')
+        page.query_selector('#app > div.modal.v--modal-overlay.scrollable > div > div.v--modal-box.v--modal > form > button').click()
+        time.sleep(3)
+        page.screenshot(path="./ScreenShots/register_user.png")
+    except Exception as e:
+        page.screenshot(path="./ScreenShots/error.png")
+        raise Exception(e)
+
 
 
 def test_login(page: Page):
